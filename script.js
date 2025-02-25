3d273471839725a56f4c55d211a8becb94722538
christina-baldwin <christina.baldwin13@yahoo.com>
2025-02-25 18:01:07 +0100

    getting the drodown functionality on ONE dropdown, TODO get it to work on all

--- script.js
+++ script.js
@@ -7,41 +7,14 @@ dropdowns.forEach((dropdown) => {
   const dropdownItems = document.querySelector(".dropdown-items");
   const dropdownOptions = document.querySelectorAll(".dropdown-option");
 
-  dropdownSelected.addEventListener("click", (element) => {
-    console.log("🚀 ~ dropdownSelected.addEventListener ~ element:", element);
-    // console.log("🚀 ~ dropdownSelected.addEventListener ~ click:", click);
-    dropdownItems.style.display = "block";
+  dropdownSelected.addEventListener("click", () => {
+    dropdownItems.classList.toggle("dropdown-open");
+  });
+
+  dropdownOptions.forEach((option) => {
+    option.addEventListener("click", () => {
+      dropdownSelected.textContent = option.textContent;
+      dropdownItems.classList.remove("dropdown-open");
+    });
   });
 });
-
-// // Attach click event listeners to each custom select
-// customSelects.forEach((select) => {
-//   console.log("🚀 ~ customSelects.forEach ~ select:", select);
-//   let selectSelected = select.querySelectorAll(".select-selected");
-//   let selectItems = select.querySelectorAll(".select-items");
-//   let options = select.querySelector(".option");
-
-//   // Toggle the dropdown visibility when the select box is clicked
-//   selectSelected.addEventListener("click", () => {
-//     if (selectItems.style.display === "none") {
-//       selectItems.style.display = "block";
-//     } else {
-//       selectItems.style.display = "none";
-//     }
-//   });
-
-//   // Set the selected option and hide the dropdown when an option is clicked
-//   options.forEach((option) => {
-//     option.addEventListener("click", () => {
-//       selectSelected.textContent = option.textContent + " ????";
-//       selectItems.style.display = "none";
-//     });
-//   });
-
-//   // Close the dropdown if the user clicks outside of it
-//   window.addEventListener("click", (e) => {
-//     if (!select.contains(e.target)) {
-//       selectItems.style.display = "none";
-//     }
-//   });
-// });
